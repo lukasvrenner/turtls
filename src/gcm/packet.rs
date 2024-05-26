43176a3b26209ea7f0d6ac34eef7a748e1d03f22
lukasvrenner <lukasvrenner@icloud.com>
2024-05-26 15:22:53 -0800

    feat: more structure around gcm packets

--- src/gcm/packet.rs
+++ src/gcm/packet.rs
@@ -1,15 +1,19 @@
+use std::array::TryFromSliceError;
+
 /// represents encryped GCM data
 pub struct Packet {
     value: Vec<u8>,
 }
 
 const MIN_PACKET_SIZE: usize = 28;
+const NONCE_SIZE: usize = 12;
+const TAG_SIZE: usize = 16;
 
 impl Packet {
 
     #[inline]
-    pub fn nonce(&self) -> &[u8] {
-        &self.value[0..16]
+    pub fn nonce(&self) -> Nonce {
+        Nonce(&self.value[0..NONCE_SIZE])
     }
 
     #[inline]
@@ -23,9 +27,9 @@ impl Packet {
     }
 
     #[inline]
-    pub fn tag(&self) -> &[u8] {
+    pub fn tag(&self) -> Tag {
         let len = self.value.len();
-        &self.value[len - 16..len]
+        Tag(&self.value[len - TAG_SIZE..len])
     }
 
     pub fn g_hash(
@@ -43,9 +47,9 @@ impl TryFrom<Vec<u8>> for Packet {
     type Error = TooShort;
 
     fn try_from(data: Vec<u8>) -> Result<Self, Self::Error> {
-        match data.len().cmp(&MIN_PACKET_SIZE) {
-            std::cmp::Ordering::Less => Err(TooShort),
-            _ => Ok(Packet { value: data }),
+        match data.len() >= MIN_PACKET_SIZE {
+            true => Ok(Packet { value: data }),
+            false => Err(TooShort),
         }
     }
 }
@@ -53,5 +57,32 @@ impl TryFrom<Vec<u8>> for Packet {
 impl Into<Vec<u8>> for Packet {
     fn into(self) -> Vec<u8> {
         self.value
+    }
+}
+
+/// a slice with a guaranteed length of `TAG_SIZE`
+struct Tag(pub &[u8]);
+
+impl TryFrom<&[u8]> for Tag {
+    type Error = TryFromSliceError;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        match value.len() == TAG_SIZE {
+            true => Ok(Tag(value)),
+            false => Err(TryFromSliceError),
+        }
+    }
+}
+
+/// a slice with a guaranteed length of `NONCE_SIZE`
+struct Nonce(pub &[u8]);
+
+impl TryFrom<&[u8]> for Nonce {
+    type Error = TryFromSliceError;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        match value.len() == NONCE_SIZE {
+            true => Ok(Tag(value)),
+            false => Err(TryFromSliceError),
+        }
     }
 }
