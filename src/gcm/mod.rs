b07661ced9bc7f27a62afe9c2f05b469de0b3ad5
lukasvrenner <lukasvrenner@icloud.com>
2024-06-02 10:11:01 -0800

    fix: fix `ghash()`
    style!: restructure gcm

--- src/gcm/mod.rs
+++ src/gcm/mod.rs
@@ -1,14 +1,11 @@
-pub mod packet;
-
 use crate::aes;
 
-use self::packet::{Packet, IV_SIZE, TAG_SIZE};
+const R: u128 = 0xe1 << 120;
 pub struct GcmCipher {
     round_keys: [[u8; aes::BLOCK_SIZE]; aes::NUM_ROUNDS + 1],
     h: u128,
 }
-
-const R: u128 = 0xe1 << 120;
+pub const IV_SIZE: usize = 12;
 
 pub struct InvalidData;
 
@@ -23,32 +20,33 @@ impl GcmCipher {
         }
     }
 
-    pub fn encrypt(&self, data: &[u8], init_vector: &[u8; IV_SIZE]) -> Packet {
-        let mut packet = Vec::with_capacity(IV_SIZE + data.len() + TAG_SIZE);
+    pub fn encrypt_inline(
+        &self,
+        data: &mut [u8],
+        add_data: &[u8],
+        init_vector: &[u8; IV_SIZE],
+    ) -> [u8; aes::BLOCK_SIZE] {
+        let counter = {
+            let mut counter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
+            counter.copy_from_slice(init_vector);
+            counter
+        };
+        self.xor_bit_stream(data, &counter);
 
-        packet.extend_from_slice(init_vector);
-
-        packet.extend_from_slice(data);
-        let len = packet.len();
-        self.xor_bit_stream(&mut packet[IV_SIZE..len], init_vector);
-
-        let tag = self.g_hash(&packet[IV_SIZE..packet.len()]);
-
-        packet.extend_from_slice(&tag);
-
-        debug_assert_eq!(packet.len(), packet.capacity());
-        packet.try_into().unwrap()
+        self.g_hash(data, add_data, &counter)
     }
 
-    pub fn decrypt(&self, packet: Packet) -> Result<Vec<u8>, InvalidData> {
-        match self.packet_is_valid(&packet) {
-            true => {
-                let mut data = packet.data().to_vec();
-                self.xor_bit_stream(&mut data, packet.init_vector());
-                Ok(data)
-            }
-            false => Err(InvalidData),
+    pub fn decrypt_inline(
+        &self,
+        data: &mut [u8],
+        add_data: &[u8],
+        counter: [u8; aes::BLOCK_SIZE],
+        tag: [u8; aes::BLOCK_SIZE],
+    ) -> Result<(), InvalidData> {
+        if !self.packet_is_valid(data, add_data, &counter, tag) {
+            return Err(InvalidData);
         }
+        Ok(())
     }
 
     pub fn generate_iv() -> [u8; IV_SIZE] {
@@ -56,42 +54,69 @@ impl GcmCipher {
     }
 
     /// produces a tag for given data
-    pub fn g_hash(&self, encrypted_data: &[u8]) -> [u8; TAG_SIZE] {
+    pub fn g_hash(
+        &self,
+        data: &[u8],
+        add_data: &[u8],
+        counter: &[u8; aes::BLOCK_SIZE],
+    ) -> [u8; aes::BLOCK_SIZE] {
         let mut tag = 0u128;
-        for chunk in encrypted_data.chunks(aes::BLOCK_SIZE) {
-            tag ^= u128::from_be_bytes(
-                match TryInto::<[u8; aes::BLOCK_SIZE]>::try_into(chunk) {
-                    Ok(array) => array,
-                    Err(_) => {
-                        let mut expanded = [0u8; aes::BLOCK_SIZE];
-                        expanded[..chunk.len()].copy_from_slice(chunk);
-                        expanded
-                    }
-                },
-            );
-            tag = gf_2to128_mult(tag, self.h);
+
+        for block in add_data.chunks_exact(aes::BLOCK_SIZE) {
+            add_block(&mut tag, block.try_into().unwrap(), self.h);
         }
-        tag ^= encrypted_data.len() as u128;
+
+        let last_block = {
+            let end = add_data.len() % aes::BLOCK_SIZE;
+            let mut last_block = [0u8; aes::BLOCK_SIZE];
+            last_block[..end]
+                .copy_from_slice(&add_data[add_data.len() - end..]);
+            last_block
+        };
+
+        add_block(&mut tag, last_block, self.h);
+
+        for block in data.chunks_exact(aes::BLOCK_SIZE) {
+            add_block(&mut tag, block.try_into().unwrap(), self.h);
+        }
+
+        let last_block = {
+            let end = data.len() % aes::BLOCK_SIZE;
+            let mut last_block = [0u8; aes::BLOCK_SIZE];
+            last_block[..end].copy_from_slice(&data[data.len() - end..]);
+            last_block
+        };
+
+        add_block(&mut tag, last_block, self.h);
+
+        tag ^= ((add_data.len() as u128 * 8) << 64) + data.len() as u128 * 8;
         tag = gf_2to128_mult(tag, self.h);
+
+        let encrypted_iv =
+            u128::from_be_bytes(aes::encrypt(counter, &self.round_keys));
+
+        tag ^= encrypted_iv;
         tag.to_be_bytes()
     }
 
     /// verifies that a give packet has not been tampered with
-    pub fn packet_is_valid(&self, packet: &Packet) -> bool {
-        self.g_hash(packet.data()) == *packet.tag()
+    pub fn packet_is_valid(
+        &self,
+        data: &[u8],
+        add_data: &[u8],
+        counter: &[u8; aes::BLOCK_SIZE],
+        tag: [u8; aes::BLOCK_SIZE],
+    ) -> bool {
+        self.g_hash(data, add_data, counter) == tag
     }
 
     /// encrypts/decrypts the data
     // use multi-threading in the future
-    fn xor_bit_stream(&self, data: &mut [u8], init_vector: &[u8; IV_SIZE]) {
-        let iv_as_int = {
-            let mut expanded_iv = [0u8; 16];
-            expanded_iv[0..init_vector.len()].copy_from_slice(init_vector);
-            u128::from_be_bytes(expanded_iv)
-        };
+    fn xor_bit_stream(&self, data: &mut [u8], counter: &[u8; aes::BLOCK_SIZE]) {
+        let iv_as_int = u128::from_be_bytes(*counter);
 
         for (counter, block) in data.chunks_mut(aes::BLOCK_SIZE).enumerate() {
-            let mut stream = (iv_as_int + 2 + counter as u128).to_be_bytes();
+            let mut stream = (iv_as_int + 1 + counter as u128).to_be_bytes();
             aes::encrypt_inline(&mut stream, &self.round_keys);
 
             for (data_byte, stream_byte) in block.iter_mut().zip(stream) {
@@ -117,6 +142,11 @@ fn gf_2to128_mult(a: u128, b: u128) -> u128 {
     product
 }
 
+fn add_block(tag: &mut u128, block: [u8; aes::BLOCK_SIZE], h: u128) {
+    *tag ^= u128::from_be_bytes(block);
+    *tag = gf_2to128_mult(*tag, h);
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -135,9 +165,9 @@ mod tests {
             0x49, 0xa6, 0xb5, 0x25, 0xb1, 0x6a, 0xed, 0xf5, 0xaa, 0x0d, 0xe6,
             0x57, 0xba, 0x63, 0x7b, 0x39, 0x1a, 0xaf, 0xd2, 0x55,
         ];
-        let initialization_vector = [
+        let counter = [
             0xca, 0xfe, 0xba, 0xbe, 0xfa, 0xce, 0xdb, 0xad, 0xde, 0xca, 0xf8,
-            0x88,
+            0x88, 0x00, 0x00, 0x00, 0x01,
         ];
         let cipher_text = [
             0x52, 0x2d, 0xc1, 0xf0, 0x99, 0x56, 0x7d, 0x07, 0xf4, 0x7f, 0x37,
@@ -148,7 +178,7 @@ mod tests {
             0x0a, 0xbc, 0xc9, 0xf6, 0x62, 0x89, 0x80, 0x15, 0xad,
         ];
         let cipher = super::GcmCipher::new(key);
-        cipher.xor_bit_stream(&mut plain_text, &initialization_vector);
+        cipher.xor_bit_stream(&mut plain_text, &counter);
         assert_eq!(plain_text, cipher_text);
     }
 
@@ -161,19 +191,33 @@ mod tests {
         ];
         let cipher = super::GcmCipher::new(key);
 
+        let counter = [
+            0xca, 0xfe, 0xba, 0xbe, 0xfa, 0xce, 0xdb, 0xad, 0xde, 0xca, 0xf8,
+            0x88, 0x00, 0x00, 0x00, 0x01,
+        ];
+
         let cipher_text = [
             0x52, 0x2d, 0xc1, 0xf0, 0x99, 0x56, 0x7d, 0x07, 0xf4, 0x7f, 0x37,
             0xa3, 0x2a, 0x84, 0x42, 0x7d, 0x64, 0x3a, 0x8c, 0xdc, 0xbf, 0xe5,
             0xc0, 0xc9, 0x75, 0x98, 0xa2, 0xbd, 0x25, 0x55, 0xd1, 0xaa, 0x8c,
             0xb0, 0x8e, 0x48, 0x59, 0x0d, 0xbb, 0x3d, 0xa7, 0xb0, 0x8b, 0x10,
             0x56, 0x82, 0x88, 0x38, 0xc5, 0xf6, 0x1e, 0x63, 0x93, 0xba, 0x7a,
-            0x0a, 0xbc, 0xc9, 0xf6, 0x62, 0x89, 0x80, 0x15, 0xad,
+            0x0a, 0xbc, 0xc9, 0xf6, 0x62,
         ];
         let tag = [
-            0xb0, 0x94, 0xda, 0xc5, 0xd9, 0x34, 0x71, 0xbd, 0xec, 0x1a, 0x50,
-            0x22, 0x70, 0xe3, 0xcc, 0x6c,
+            0x76, 0xfc, 0x6e, 0xce, 0x0f, 0x4e, 0x17, 0x68, 0xcd, 0xdf, 0x88,
+            0x53, 0xbb, 0x2d, 0x55, 0x1b,
         ];
-        assert_eq!(tag, cipher.g_hash(&cipher_text));
+
+        let add_data = [
+            0xfe, 0xed, 0xfa, 0xce, 0xde, 0xad, 0xbe, 0xef, 0xfe, 0xed, 0xfa,
+            0xce, 0xde, 0xad, 0xbe, 0xef, 0xab, 0xad, 0xda, 0xd2,
+        ];
+
+        let h = 0xacbef20579b4b8ebce889bac8732dad7;
+        assert_eq!(cipher.h, h);
+
+        assert_eq!(tag, cipher.g_hash(&cipher_text, &add_data, &counter));
     }
 
     #[test]
