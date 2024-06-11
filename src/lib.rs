4ac0ab9370166e5df5e7926e19d0504a6af3e5ce
lukasvrenner <lukasvrenner@icloud.com>
2024-06-11 10:17:23 -0800

    docs: updated docs
    docs: update and add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! This crate implements various cryptographic functions,
+//! including AES in GCM and SHA-256.
+//!
+//! This crate implements these functions purely in software.
+//! Hardware implementations are a future goal.
+//!
+//! WARNING: This code has not been audited. Use at your own risk.
+//!
+//! This crate has the long-term goal of 
+//! becoming a fully compliant TLS 1.3 library, with C bindings.
 #![warn(missing_docs)]
 pub mod aes;
 pub mod gcm;
