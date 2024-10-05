baa57b3cee4275eec50efe13900c099fbfec3a23
lukasvrenner <lukasvrenner@icloud.com>
2024-10-05 12:44:23 -0800

    feat: add hasher traits and make hmac generic

--- libcrypto/src/sha2.rs
+++ libcrypto/src/sha2.rs
@@ -1,8 +1,22 @@
 //! The SHA2 family of hash functions.
-mod hmac_sha256;
+mod hmac;
 mod sha256;
 mod sha512;
 
-pub use hmac_sha256::hmac_sha256;
+pub use hmac::hmac;
 pub use sha256::Sha256;
 pub use sha512::Sha512;
+
+pub trait Hasher<const HASH_SIZE: usize> {
+    fn new() -> Self;
+
+    fn update_with_msg(&mut self, msg: &[u8]);
+
+    fn finalize(self) -> [u8; HASH_SIZE];
+
+    fn hash() -> [u8; HASH_SIZE];
+}
+
+pub trait BlockHasher<const H_SIZE: usize, const B_SIZE: usize>: Hasher<H_SIZE> {
+    fn update_with(&mut self, block: &[u8; B_SIZE]);
+}
--- libcrypto/src/sha2/hmac.rs
+++ libcrypto/src/sha2/hmac.rs
@@ -1,20 +1,23 @@
-use super::sha256::Sha256;
+use super::BlockHasher;
 
-pub fn hmac_sha256(msg: &[u8], key: &[u8]) -> [u8; Sha256::HASH_SIZE] {
-    let mut ipad = [0x36; Sha256::BLOCK_SIZE];
-    let mut opad = [0x5c; Sha256::BLOCK_SIZE];
+pub fn hmac<const B_SIZE: usize, const H_SIZE: usize, H: BlockHasher<H_SIZE, B_SIZE>>(
+    msg: &[u8],
+    key: &[u8],
+) -> [u8; H_SIZE] {
+    let mut ipad = [0x36; B_SIZE];
+    let mut opad = [0x5c; B_SIZE];
 
     for ((ipad_byte, opad_byte), key_byte) in ipad.iter_mut().zip(opad.iter_mut()).zip(key) {
         *ipad_byte ^= key_byte;
         *opad_byte ^= key_byte;
     }
 
-    let mut inner_hasher = Sha256::new();
+    let mut inner_hasher = H::new();
     inner_hasher.update_with(&ipad);
     inner_hasher.update_with_msg(msg);
     let inner_hash = inner_hasher.finalize();
 
-    let mut hasher = Sha256::new();
+    let mut hasher = H::new();
     hasher.update_with(&opad);
     hasher.update_with_msg(&inner_hash);
 
