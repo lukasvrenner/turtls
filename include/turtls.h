376194fc93c144ffc2a47cf93a7e1fb1a9813204
lukasvrenner <lukasvrenner@icloud.com>
2024-10-13 20:40:46 -0800

    fix: add header guards for turtls.h

--- include/turtls.h
+++ include/turtls.h
@@ -1,5 +1,8 @@
+#ifndef TURTLS_H
+#define TURTLS_H
 #include <sys/types.h>
 #include <stddef.h>
 struct State;
 
 struct State *shake_hands(ssize_t (*write)(void *, size_t), ssize_t (*read)(void *, size_t));
+#endif
