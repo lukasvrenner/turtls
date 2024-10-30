673a1d598fd723098bb25c447c712df8f4152f75
lukasvrenner <lukasvrenner@icloud.com>
2024-10-29 18:48:20 -0800

    style: seperate operations into their own `unsafe` blocks

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,4 @@
 use std::mem::MaybeUninit;
-use std::sync::RwLock;
 use std::time::Duration;
 
 use crate::aead::{AeadReader, AeadWriter};
@@ -29,17 +28,20 @@ impl State {
         io: Io,
     ) -> &mut RecordLayer {
         let state_ptr = state.as_mut_ptr();
-        // SAFETY: a MaybeUninit<State> has a valid MaybeUninit<RecordLayer>
-        let buf_ptr =
-            unsafe { &mut *(&raw mut (*state_ptr).msg_buf as *mut MaybeUninit<RecordLayer>) };
-        RecordLayer::init(buf_ptr, msg_type, io)
+        // SAFETY: `MaybeUninit<T>` has the same memory layout as `T` so we can
+        // access pointers to fields as long as we cast the pointer back into a `MaybeUninit`.
+        let buf_ptr = unsafe { &raw mut (*state_ptr).msg_buf as *mut MaybeUninit<RecordLayer> };
+        // SAFETY: The pointer was just grabbed from a valid field.
+        let buf_ref = unsafe { &mut *buf_ptr };
+        RecordLayer::init(buf_ref, msg_type, io)
     }
 
     pub fn get_uninit_config(state: &mut MaybeUninit<Self>) -> &mut MaybeUninit<Config> {
-        // SAFETY: a MaybeUninit<State> has a valid MaybeUninit<Config>
-        unsafe {
-            let config_ptr = &raw mut (*state.as_mut_ptr()).config.read_timeout;
-            &mut *(config_ptr as *mut MaybeUninit<Config>)
-        }
+        let ptr = state.as_mut_ptr();
+        // SAFETY: `MaybeUninit<T>` has the same memory layout as `T` so we can
+        // access pointers to fields as long as we cast the pointer back into a `MaybeUninit`.
+        let config_ptr = unsafe { &raw mut (*ptr).config.read_timeout as *mut MaybeUninit<Config> };
+        // SAFETY: The pointer was just grabbed from a valid field.
+        unsafe { &mut *config_ptr }
     }
 }
